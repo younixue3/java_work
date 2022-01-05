43afe59299c688158a63f0b7607d389d85aeecf5
younixue3 <bussiness.needs.rctka@gmail.com>
2022-01-05 10:56:36 +0800

    first commit

---
+++ PhoneUser.java
@@ -0,0 +1,23 @@
+public class PhoneUser {
+    private Phone phone;
+
+    public PhoneUser(Phone phone) {
+        this.phone = phone;
+    }
+
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
